b1ac4ea2f2317774179dcf14f14aaf8ba3177878
Shitosh Parajuli <shitosh@yahoo.com>
2017-01-24 16:01:08 -0500

    documentation

--- 1.rkt
+++ 1.rkt
@@ -1,7 +1,18 @@
+;Function to reverse a list and its sublists
 (DEFINE (reverse-general L)
         (COND
-         ((NULL? L) L)
-         ((LIST? (CAR L)) (append (reverse-general (CDR L)) (LIST (reverse-general(CAR L)))))
-         (ELSE  (append (reverse-general (CDR L)) (list (CAR L))))
-        )
+         ((NULL? L) L)   ;if list is null, return the list as it is
+          ;if the first element of the list a list too, recursively
+          ;reverse the remaining part, and append the reverse of first part to last
+         ((LIST? (CAR L))     
+          (append (reverse-general (CDR L))
+                  (LIST (reverse-general(CAR L)))
+           )
+          )
+         ;else, if first part is an atom, take reverse of remaining and append that atom
+         (ELSE
+          (append (reverse-general (CDR L))
+                  (list (CAR L)))
+         )
+         )
 )
