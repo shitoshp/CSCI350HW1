a59d60b43f3dd7509931fd82f71a3c3338d7b843
Shitosh Parajuli <shitosh@yahoo.com>
2017-01-24 12:05:29 -0500

    reverses everything but removes internal parentheses

--- 1.rkt
+++ 1.rkt
@@ -1,6 +1,8 @@
 (DEFINE (reverse-general L)
-        (IF (NULL? (CDR L))
+        (IF (NULL? L)
             L
-            (append (reverse-general (CDR L)) (LIST (CAR L)))
+            (append (reverse-general (CDR L)) (IF (LIST? (CAR L))
+                                                  (reverse-general (CAR L))
+                                                  (list (CAR L))))
         )
 )
