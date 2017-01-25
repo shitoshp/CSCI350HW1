7d3f278d538a1165e433d438fbf77741bce285f8
Shitosh Parajuli <shitosh@yahoo.com>
2017-01-25 01:05:07 -0500

    Documentation

--- 2.rkt
+++ 2.rkt
@@ -1,8 +1,10 @@
 ;Function to find sum of numbers not in nested list in a list L
 (DEFINE (sum-up-numbers-simple L)
         (COND
-         ((NULL? L) 0)
-         ((NUMBER? (CAR L))  (+ (CAR L) (sum-up-numbers-simple (CDR L))))
-         (ELSE (+ 0 (sum-up-numbers-simple (CDR L))))
+         ((NULL? L) 0)     ;if list is null, sum is 0
+         ((NUMBER? (CAR L))  ;if next element is a number, take sum of that element and recursively of remaining
+              (+ (CAR L) (sum-up-numbers-simple (CDR L))))
+         (ELSE      ;if it is not a number and not null, add 0 to sum, sum remains unchanged
+              (+ 0 (sum-up-numbers-simple (CDR L))))
          )
 )
