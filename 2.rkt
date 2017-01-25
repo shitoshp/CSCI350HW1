fd1656578ad170c3faa15ef848d97c92677c80e8
Shitosh Parajuli <shitosh@yahoo.com>
2017-01-24 20:53:44 -0500

    number check to discard char and lists

--- 2.rkt
+++ 2.rkt
@@ -1,7 +1,7 @@
 ;Function to find sum of numbers not in nested list in a list L
 (DEFINE (sum-up-numbers-simple L)
-        (IF (NULL? L)
-            0
-            (+ (CAR L) (sum-up-numbers-simple (CDR L)))
+        (COND
+         ((NULL? L) 0)
+         ((NUMBER? (CAR L))  (+ (CAR L) (sum-up-numbers-simple (CDR L))))
          )
 )
