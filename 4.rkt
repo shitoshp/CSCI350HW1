91009f94bd32457ded1a2c106bc15f5d36abfa6a
Shitosh Parajuli <shitosh@yahoo.com>
2017-01-27 14:28:08 -0500

    remove smaller numbers

--- 4.rkt
+++ 4.rkt
@@ -20,8 +20,14 @@
 ;function to remove non-numbers and numbers below min from list1
 (DEFINE (remove-min list1 list2)
         (COND
+         ;if list1 is null, return null
          ((NULL? list1) list1)
+         ;if list2 min is null, return all numbers of list1
+         ((NULL? (list-minimum list2)) (NOT (NUMBER? (CAR list1))) (remove-min (CDR list1)))
+         ;if not a number, don't add
          ((NOT (NUMBER? (CAR list1))) (remove-min (CDR list1)))
+         ;if each num of list1 is greater than min of list2, add to returning
+         ((> (CAR list1) (list-minimum list2)) (CONS (CAR list1) (remove-min(CDR list1))))
          
          )
 )
