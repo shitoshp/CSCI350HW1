f976fefdd9430b032389cb084ac99fc5aaaa3991
Shitosh Parajuli <shitosh@yahoo.com>
2017-01-27 10:46:36 -0500

    minimum helper function: base case/for single element list

---
+++ 4.rkt
@@ -0,0 +1,9 @@
+;helper function to find minimum value of a list
+(DEFINE (minimum L)
+        (COND
+         ;if only 1 element remaining in list, it must be minimum
+         ((NULL? (CDR L)) (CAR L))
+         
+         )
+
+)
