98328a251b04ce3352f3fd730c30b52e94ac3c8c
Shitosh Parajuli <shitosh@yahoo.com>
2017-01-26 21:33:04 -0500

    comments added

--- 3.rkt
+++ 3.rkt
@@ -4,6 +4,7 @@
          ((NULL? L) 0)     ;if list is null, sum is 0
          ((NUMBER? (CAR L))  ;if next element is a number, take sum of that element and recursively of remaining
               (+ (CAR L) (sum-up-numbers-simple (CDR L))))
+         ;if next element is a list, recursively take sum of the list and add with remaining
          ((LIST? (CAR L))
               (+ (sum-up-numbers-simple (CAR L)) (sum-up-numbers-simple (CDR L))))
          (ELSE      ;if it is not a number and not null, add 0 to sum, sum remains unchanged
