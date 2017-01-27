5463d9548772696744f0215eaf4333c29665a15d
Shitosh Parajuli <shitosh@yahoo.com>
2017-01-27 11:11:34 -0500

    working for non-empty list of numbers

--- 4.rkt
+++ 4.rkt
@@ -1,9 +1,12 @@
 ;helper function to find minimum value of a list
-(DEFINE (minimum L)
+(DEFINE (list-minimum L)
         (COND
          ;if only 1 element remaining in list, it must be minimum
          ((NULL? (CDR L)) (CAR L))
-         
+         ;recursively compare first element with minimum of remaining
+         ;if first element is small, it is minimum
+         ((< (CAR L) (list-minimum (CDR L))) (CAR L))
+         ;else, minimum of remaining list is minimum
+         (ELSE (list-minimum (CDR L)))
          )
-
 )
